a65767ee285fdcc775162d4f4e1b8b535409551f
Ping Zhang <pizhang@gmail.com>
2025-03-03 06:58:13 +1100

    Added CloudShell commands to demonstrate SQS queue message handling

--- SQS/sqs-demo-consumer-manual.py
+++ SQS/sqs-demo-consumer-manual.py
@@ -2,7 +2,7 @@ import boto3
 
 sqs = boto3.client('sqs', region_name='ap-southeast-2')
 
-queue_url = "https://sqs.ap-southeast-2.amazonaws.com/509399591785/basic-demo"
+queue_url = <Your SQS Queue URL>
 response = sqs.receive_message(QueueUrl=queue_url)
 
 print(response)
