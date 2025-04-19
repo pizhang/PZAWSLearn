0fce83ffc6b6c55ef76ddddc384dc8cb5365c629
Ping Zhang <92210220+pizhang@users.noreply.github.com>
2025-04-19 10:46:40 +1000

    Added Terraform training
    Added Terraform training

---
+++ TerraformLearn/01-basics/web-app/main.tf
@@ -0,0 +1,205 @@
+terraform {
+  # Assumes s3 bucket and dynamo DB table already set up
+  # See /code/03-basics/aws-backend
+  backend "s3" {
+    bucket         = "tf-state-509399591785-ap-southeast-2"
+    key            = "01-basics/web-app/terraform.tfstate"
+    region         = "ap-southeast-2"
+    dynamodb_table = "terraform-state-locking"
+    encrypt        = true
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-southeast-2"
+}
+
+data "aws_subnet" "public1" {
+    id = "subnet-01413e0a98924db46"
+}
+
+data "aws_subnet" "public2" {
+    id = "subnet-04ffaef97ea077eb2"
+}
+
+data "aws_vpc" "selected" {
+  id = "vpc-0644f0ed39837c87a"
+}
+
+data "aws_subnets" "public" {
+    filter {
+        name = "vpc-id"
+        values = [data.aws_vpc.selected.id]
+    }
+    
+    tags = {
+        Tier ="public"
+    }
+}
+
+resource "aws_security_group" "instances" {
+  name = "tf01-ec2-security-group"
+  vpc_id = data.aws_vpc.selected.id
+}
+
+resource "aws_instance" "instance_1" {
+  ami             = var.ami
+  instance_type   = "t2.micro"
+  subnet_id = data.aws_subnet.public1.id
+  security_groups = [aws_security_group.instances.id]
+  user_data       = <<-EOF
+              #!/bin/bash
+              echo "Hello, World 1" > index.html
+              python3 -m http.server 8080 &
+              EOF
+}
+
+resource "aws_instance" "instance_2" {
+  ami             = var.ami
+  instance_type   = "t2.micro"
+  subnet_id = data.aws_subnet.public2.id
+  security_groups = [aws_security_group.instances.id]
+  user_data       = <<-EOF
+              #!/bin/bash
+              echo "Hello, World 2" > index.html
+              python3 -m http.server 8080 &
+              EOF
+}
+
+resource "aws_s3_bucket" "bucket" {
+  bucket_prefix = "tf01-web-app"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_versioning" "bucket_versioning" {
+  bucket = aws_s3_bucket.bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "bucket_crypto_conf" {
+  bucket = aws_s3_bucket.bucket.bucket
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_security_group_rule" "allow_http_inbound" {
+  type              = "ingress"
+  security_group_id = aws_security_group.instances.id
+
+  from_port   = 8080
+  to_port     = 8080
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.load_balancer.arn
+
+  port = 80
+
+  protocol = "HTTP"
+
+  # By default, return a simple 404 page
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code  = 404
+    }
+  }
+}
+
+resource "aws_lb_target_group" "instances" {
+  name     = "example-target-group"
+  port     = 8080
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.selected.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_target_group_attachment" "instance_1" {
+  target_group_arn = aws_lb_target_group.instances.arn
+  target_id        = aws_instance.instance_1.id
+  port             = 8080
+}
+
+resource "aws_lb_target_group_attachment" "instance_2" {
+  target_group_arn = aws_lb_target_group.instances.arn
+  target_id        = aws_instance.instance_2.id
+  port             = 8080
+}
+
+resource "aws_lb_listener_rule" "instances" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 100
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.instances.arn
+  }
+}
+
+
+resource "aws_security_group" "alb" {
+  name = "tf01-alb-security-group"
+  vpc_id = data.aws_vpc.selected.id
+}
+
+resource "aws_security_group_rule" "allow_alb_http_inbound" {
+  type              = "ingress"
+  security_group_id = aws_security_group.alb.id
+
+  from_port   = 80
+  to_port     = 80
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+
+}
+
+resource "aws_security_group_rule" "allow_alb_all_outbound" {
+  type              = "egress"
+  security_group_id = aws_security_group.alb.id
+
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+
+}
+
+resource "aws_lb" "load_balancer" {
+  name               = "web-app-lb"
+  load_balancer_type = "application"
+  subnets            = data.aws_subnets.public.ids
+  security_groups    = [aws_security_group.alb.id]
+
+}
---
+++ TerraformLearn/01-basics/web-app/variables.tf
@@ -0,0 +1,4 @@
+variable "ami" {
+    type = string
+    default = "ami-0f5d1713c9af4fe30"  
+}
